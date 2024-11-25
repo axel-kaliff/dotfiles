be3948085049e53ee466d7afc6202adffd381242
Axel Kaliff <axel.kaliff@protonmail.com>
2024-11-25 17:25:34 +0100

    remove emoji plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -124,31 +124,6 @@ require('lazy').setup({
     },
   },
 
-  ------ Emoji
-  {
-    "allaman/emoji.nvim",
-    version = "1.0.0", -- optionally pin to a tag
-    ft = "markdown", -- adjust to your needs
-    dependencies = {
-      -- optional for nvim-cmp integration
-      "hrsh7th/nvim-cmp",
-      -- optional for telescope integration
-      "nvim-telescope/telescope.nvim",
-    },
-    opts = {
-      -- default is false
-      enable_cmp_integration = true,
-      -- optional if your plugin installation directory
-      -- is not vim.fn.stdpath("data") .. "/lazy/
-      plugin_path = vim.fn.expand("$HOME/plugins/"),
-    },
-    config = function(_, opts)
-      require("emoji").setup(opts)
-      -- optional for telescope integration
-      local ts = require('telescope').load_extension 'emoji'
-      vim.keymap.set('n', '<leader>se', ts.emoji, { desc = '[S]earch [E]moji' })
-    end,
-  },
 
   -- dashboard
   {
@@ -497,6 +472,7 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+
 
 -- Configure nvim-cmp
 local cmp = require('cmp')
