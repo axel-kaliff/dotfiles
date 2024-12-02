5053b8af9f0b09a21b52b2f5aa0ff85d9066a67b
Axel Kaliff <axel.kaliff@protonmail.com>
2024-12-02 22:25:05 +0100

    Update dotfiles

--- startup.fish
+++ startup.fish
@@ -1,15 +1,9 @@
 #!/usr/bin/env fish
 
-# Step 1: Perform git pull in the ~/dotfiles directory
-if test -d ~/dotfiles
-    cd ~/dotfiles
-    git pull
-else
-    echo "Directory ~/dotfiles does not exist."
-end
+# TODO add this to atuin
+udot
 
 # Step 2: Start flatpaks
 flatpak run md.obsidian.Obsidian &
 flatpak run io.github.slgobinath.SafeEyes &
 
-udot
