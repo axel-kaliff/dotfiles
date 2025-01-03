25fe3c49d29b4b50925f0edb558146a718912b79
Axel Kaliff <axel.kaliff@protonmail.com>
2025-01-03 10:51:52 +0100

    Update dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -175,20 +175,6 @@ require('lazy').setup({
     end,
   },
 
-  -- {
-  --   -- Theme inspired by Atom
-  --   'navarasu/onedark.nvim',
-  --   priority = 1000,
-  --   lazy = false,
-  --   config = function()
-  --     require('onedark').setup {
-  --       -- Set a style preset. 'dark' is default.
-  --       style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-  --     }
-  --     require('onedark').load()
-  --   end,
-  -- },
-
   {
     "NoahTheDuke/vim-just",
     ft = { "just" },
