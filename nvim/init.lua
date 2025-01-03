e97ecba3ac9c2e90c89feaca9f9634ea734f78d6
Axel Kaliff <axel.kaliff@protonmail.com>
2025-01-03 10:46:25 +0100

    Update dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -196,7 +196,6 @@ require('lazy').setup({
 
   {
     "EdenEast/nightfox.nvim",
-    load = 'nordfox'
   },
 
 
@@ -448,7 +447,7 @@ wk.add({ { "<leader>", group = "VISUAL <leader>", mode = "v" }, { "<leader>h", d
 require('mason').setup()
 require('mason-lspconfig').setup()
 
-require('nightfox').load()
+require('nightfox').load("nordfox")
 
 local servers = {
   gopls = {},
