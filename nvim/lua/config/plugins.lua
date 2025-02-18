065ddf643ea42d8fd149db07151aab314e778b4d
Axel Kaliff <axel.kaliff@protonmail.com>
2025-02-18 14:37:33 +0100

    Update dotfiles

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -156,25 +156,25 @@ require('lazy').setup({
           lualine_y = {},
           lualine_z = { 'tabs' }
         },
-        sections = {
-          -- these are to remove the defaults
-          lualine_a = {},
-          lualine_b = {},
-          lualine_y = {},
-          lualine_z = {},
-          -- These will be filled later
-          lualine_c = {},
-          lualine_x = {},
-        },
-        -- inactive_sections = {
+        -- sections = {
         --   -- these are to remove the defaults
         --   lualine_a = {},
         --   lualine_b = {},
         --   lualine_y = {},
         --   lualine_z = {},
+        --   -- These will be filled later
         --   lualine_c = {},
         --   lualine_x = {},
         -- },
+        inactive_sections = {
+          -- these are to remove the defaults
+          lualine_a = {},
+          lualine_b = {},
+          lualine_y = {},
+          lualine_z = {},
+          lualine_c = {},
+          lualine_x = {},
+        },
       },
     },
     {
