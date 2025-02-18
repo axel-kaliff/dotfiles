4b6ae1f3b9cce829773c56a21a289f44e69692d9
Axel Kaliff <axel.kaliff@protonmail.com>
2025-02-18 15:08:36 +0100

    Update dotfiles

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -370,5 +370,8 @@ require("bufferline").setup({
         separator = true
       }
     },
+    numbers = function(opts)
+      return string.format('%s·%s', opts.raise(opts.id), opts.lower(opts.ordinal))
+    end,
   }
 })
