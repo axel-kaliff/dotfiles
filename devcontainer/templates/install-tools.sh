bcc6b20c0df40ae0d784c74d0dddffb8d1665427
Axel Kaliff <axel.kaliff@protonmail.com>
2025-03-19 10:01:14 +0100

    Update dotfiles

--- devcontainer/templates/install-tools.sh
+++ devcontainer/templates/install-tools.sh
@@ -7,6 +7,11 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.2.1/zsh-in-docker.sh)"
 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit -D -t /usr/local/bin/
+
 echo 'eval "$(uv generate-shell-completion bash)"' >> /root/.bashrc
 echo 'eval "$(uv generate-shell-completion zsh)"' >> /root/.zshrc
 echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> /root/.bashrc
