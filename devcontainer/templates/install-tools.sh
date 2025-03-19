34945bd37b53ce834d29e5022ae98bf17dd26858
Axel Kaliff <axel.kaliff@protonmail.com>
2025-03-19 01:39:32 +0100

    cleanup

--- devcontainer/templates/install-tools.sh
+++ devcontainer/templates/install-tools.sh
@@ -1,11 +1,5 @@
-# install developer tools
 curl -LsSf https://astral.sh/uv/install.sh | sh
 curl -fsSL https://pixi.sh/install.sh | bash
-
-echo 'eval "$(uv generate-shell-completion bash)"' >> /root/.bashrc
-
-
-echo 'eval "$(uv generate-shell-completion zsh)"' >> /root/.zshrc
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz && \
                 rm -rf /opt/nvim && \
@@ -13,5 +7,6 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.2.1/zsh-in-docker.sh)"
 
+echo 'eval "$(uv generate-shell-completion bash)"' >> /root/.bashrc
+echo 'eval "$(uv generate-shell-completion zsh)"' >> /root/.zshrc
 echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> /root/.bashrc
-
