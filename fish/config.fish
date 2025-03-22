21bd9f9a2b156d283defa188b604a3278a833ce8
Axel Kaliff <axel.kaliff@protonmail.com>
2025-03-22 13:58:51 +0100

    Update dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -88,6 +88,15 @@ end
 # bind \cs '__ethp_commandline_toggle_sudo.fish'
 bind \cs '__ethp_commandline_toggle_sudo'
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 
 export EDITOR=nvim
 
