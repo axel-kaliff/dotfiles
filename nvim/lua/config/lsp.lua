d1c80672b2d969e2e2da466e265ed62d1ff6fcae
Axel Kaliff <axel.kaliff@protonmail.com>
2025-03-27 12:57:44 +0100

    Update dotfiles

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -54,8 +54,6 @@ local lspconfig = require('lspconfig')
 lspconfig['lua_ls'].setup({ capabilities = capabilities })
 
 require('neodev').setup()
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local mason_lspconfig = require('mason-lspconfig')
 mason_lspconfig.setup { ensure_installed = vim.tbl_keys(servers) }
--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -370,10 +370,6 @@ require('lazy').setup({
 
       -- use a release tag to download pre-built binaries
       version = '1.*',
-      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-      -- build = 'cargo build --release',
-      -- If you use nix, you can build from source using latest nightly rust with:
-      -- build = 'nix run .#build-plugin',
 
       ---@module 'blink.cmp'
       ---@type blink.cmp.Config
@@ -399,7 +395,7 @@ require('lazy').setup({
         },
 
         -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+        completion = { documentation = { auto_show = true } },
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
