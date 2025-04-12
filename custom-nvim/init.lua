bc7722c5fa09e6f965ee12b0740cdfd9f54eede3
Axel Kaliff <axel.kaliff@protonmail.com>
2025-04-12 15:53:55 +0200

    Update dotfiles

---
+++ custom-nvim/init.lua
@@ -0,0 +1,18 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Load configuration modules
+require('config.options')
+require('config.plugins')
+require('config.keymaps')
+require('config.autocmds')
+require('config.telescope')
+require('config.treesitter')
+require('config.lsp')
+-- require('config.cmp')
+require('config.whichkey')
+require('config.dashboard')
+
+-- Modeline
+-- vim: ts=2 sts=2 sw=2 et
