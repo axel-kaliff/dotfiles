8287a173e843aa3c112aff4929529df395a8b9ed
Axel Kaliff <axel.kaliff@protonmail.com>
2025-04-15 10:42:57 +0200

    Update dotfiles

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -76,6 +76,133 @@ return {
       end,
     },
 
+    -- Bufferline
+    {
+      'akinsho/bufferline.nvim',
+      version = "*",
+      dependencies = 'nvim-tree/nvim-web-devicons',
+      config = function()
+
+      end,
+    },
+
+    { 'numToStr/Comment.nvim', opts = {} },
+    {
+      'nvim-telescope/telescope.nvim',
+      branch = '0.1.x',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+          'nvim-telescope/telescope-fzf-native.nvim',
+          build = 'make',
+          cond = function() return vim.fn.executable('make') == 1 end,
+        },
+      },
+    },
+
+    ----------------------------------------------------------------------------
+    -- Debugger configuration: nvim-dap and related plugins
+    ----------------------------------------------------------------------------
+    {
+      'mfussenegger/nvim-dap',
+      dependencies = {
+        'rcarriga/nvim-dap-ui',
+        'nvim-neotest/nvim-nio',
+        'williamboman/mason.nvim',
+        'jay-babu/mason-nvim-dap.nvim',
+        'mfussenegger/nvim-dap-python',
+      },
+      keys = {
+        {
+          '<F5>',
+          function() require('dap').continue() end,
+          desc = 'Debug: Start/Continue',
+        },
+        {
+          '<F1>',
+          function() require('dap').step_into() end,
+          desc = 'Debug: Step Into',
+        },
+        {
+          '<F2>',
+          function() require('dap').step_over() end,
+          desc = 'Debug: Step Over',
+        },
+        {
+          '<F3>',
+          function() require('dap').step_out() end,
+          desc = 'Debug: Step Out',
+        },
+        {
+          '<leader>b',
+          function() require('dap').toggle_breakpoint() end,
+          desc = 'Debug: Toggle Breakpoint',
+        },
+        {
+          '<leader>B',
+          function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end,
+          desc = 'Debug: Set Breakpoint',
+        },
+        {
+          '<F7>',
+          function() require('dapui').toggle() end,
+          desc = 'Debug: See last session result.',
+        },
+      },
+      config = function()
+        local dap = require('dap')
+
+        -- dap.adapters.python = {
+        --   type = 'executable';
+        --   command = os.getenv('HOME') .. '/.virtualenvs/tools/bin/python';
+        --   args = { '-m', 'debugpy.adapter' };
+        -- }
+
+        -- dap.configurations.python = {
+        --   {
+        --     type = 'python',
+        --     request = 'launch',
+        --     name = "Launch file",
+        --     program = "${file}",
+        --     pythonPath = function()
+        --       return '/workspaces/'
+        --     end,
+        --   },
+        -- }
+
+
+        local dapui = require('dapui')
+
+        require('mason-nvim-dap').setup {
+          automatic_installation = true,
+          handlers = {},
+          ensure_installed = { 'delve' },
+        }
+
+        dapui.setup {
+          icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+          controls = {
+            icons = {
+              pause      = '⏸',
+              play       = '▶',
+              step_into  = '⏎',
+              step_over  = '⏭',
+              step_out   = '⏮',
+              step_back  = 'b',
+              run_last   = '▶▶',
+              terminate  = '⏹',
+              disconnect = '⏏',
+            },
+          },
+        }
+
+        dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+        dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+        dap.listeners.before.event_exited['dapui_config']     = dapui.close
+
+        require('dap-python').setup("uv")
+      end,
+    },
 
 
 
