f5fd1168057884036b285500a1aac8ef0025f446
Axel Kaliff <axel.kaliff@protonmail.com>
2025-04-16 12:40:57 +0200

    Update dotfiles

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -114,4 +114,12 @@ return {
   },
 
   { 'EdenEast/nightfox.nvim' },
+
+  require('lspconfig').ruff.setup {
+    init_options = {
+      settings = {
+        -- Ruff language server settings go here
+      },
+    },
+  },
 }
