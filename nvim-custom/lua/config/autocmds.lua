7a0b22cc419e1f480bc6e531c3750384011bd8ca
Axel Kaliff <axel.kaliff@protonmail.com>
2025-05-20 10:38:16 +0200

    Update dotfiles

--- nvim-custom/lua/config/autocmds.lua
+++
@@ -1,7 +0,0 @@
--- Highlight on yank
-local yank_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group    = yank_group,
-  pattern  = '*',
-  callback = function() vim.highlight.on_yank() end,
-})
