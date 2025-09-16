d6105f93bac363ff58645ee7c8aa6f910f559a68
Axel Kaliff <axel.kaliff@protonmail.com>
2025-09-16 14:51:12 +0200

    Update dotfiles

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -94,10 +94,23 @@ return {
   },
 
   -- tmux nvim navigation
+  -- {
+  --   'aserowy/tmux.nvim',
+  --   config = function()
+  --     require('tmux').setup {}
+  --   end,
+  -- },
+
   {
-    'aserowy/tmux.nvim',
+    'https://github.com/swaits/zellij-nav.nvim',
     config = function()
-      require('tmux').setup {}
+      require('zellij-nav').setup()
+
+      local map = vim.keymap.set
+      map('n', '<c-h>', '<cmd>ZellijNavigateLeftTab<cr>', { desc = 'navigate left or tab' })
+      map('n', '<c-j>', '<cmd>ZellijNavigateDown<cr>', { desc = 'navigate down' })
+      map('n', '<c-k>', '<cmd>ZellijNavigateUp<cr>', { desc = 'navigate up' })
+      map('n', '<c-l>', '<cmd>ZellijNavigateRightTab<cr>', { desc = 'navigate right or tab' })
     end,
   },
 
