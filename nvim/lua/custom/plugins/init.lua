f48ea4d72bfafc037d53630dac4573df90b07e57
Axel Kaliff <axel.kaliff@protonmail.com>
2025-11-29 16:28:01 +0100

    Update dotfiles

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -33,7 +33,7 @@ return {
     config = function()
       require('neo-tree').setup {
         close_if_last_window = true,
-        -- popup_border_style = 'rounded',
+        popup_border_style = 'rounded',
         enable_git_status = true,
         enable_diagnostics = false,
         source_selector = {
@@ -66,6 +66,26 @@ return {
             mappings = {
               f = 'none',
             },
+          },
+        },
+      }
+    end,
+  },
+
+  {
+    's1n7ax/nvim-window-picker',
+    version = '2.*',
+    config = function()
+      require('window-picker').setup {
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { 'terminal', 'quickfix' },
           },
         },
       }
