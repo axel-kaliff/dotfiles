b5f0e7ba60919deaffbdd4528998a3a8770cb13b
Axel Kaliff <axel.kaliff@protonmail.com>
2025-11-29 18:00:51 +0100

    Update dotfiles

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -127,7 +127,7 @@ return {
       require('zellij-nav').setup()
 
       local map = vim.keymap.set
-      map('n', '<c-h>', '<cmd>ZellijNavigateLeftTab<cr>', { desc = 'navigate left or tab' })
+      map('n', '<c-h>', '<cmd>Centerpad<cr><cmd>ZellijNavigateLeftTab<cr><cmd>Centerpad<cr>', { desc = 'navigate left or tab' })
       map('n', '<c-j>', '<cmd>ZellijNavigateDown<cr>', { desc = 'navigate down' })
       map('n', '<c-k>', '<cmd>ZellijNavigateUp<cr>', { desc = 'navigate up' })
       map('n', '<c-l>', '<cmd>ZellijNavigateRightTab<cr>', { desc = 'navigate right or tab' })
