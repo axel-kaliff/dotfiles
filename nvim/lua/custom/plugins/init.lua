50b315339ffcc521a80dd927aa35a2fcbb6e852b
Axel Kaliff <axel.kaliff@protonmail.com>
2026-03-04 16:09:54 +0100

    Update dotfiles

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,15 +1,6 @@
 return {
 
   {
-    'nosduco/remote-sshfs.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-    opts = {
-      -- Refer to the configuration section below
-      -- or leave empty for defaults
-    },
-  },
-
-  {
     'nvimdev/dashboard-nvim',
     event = 'VimEnter',
     dependencies = { { 'nvim-tree/nvim-web-devicons' } },
